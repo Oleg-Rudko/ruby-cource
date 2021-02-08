429d23e3d94c6d8dd4ff70ed6ef1df34eb2a2bcc
Oleg Rudko <oleg.rudko0906@gmail.com>
2021-02-08 15:52:41 +0200

    Work on after in rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :price, :weight,  numireacality: { greater_than: 0, allow_nil: true }
+  validates :name, presence :true
+
+  after_initialize { p 'initalize'} #Item.first
+  after_create     { p 'create'}    #Item.create => create and save
+  after_save       { p 'save'}      #Item.save
+  after_update     { p 'update'}    #Item.update => update and save
+  after_destroy    { p 'destroy'}   #Item.destroy
 end
