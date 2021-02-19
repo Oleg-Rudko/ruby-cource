3273fd57a0698a51f8e48071fd65d72d242e9a55
Oleg Rudko <oleg.rudko0906@gmail.com>
2021-02-19 14:05:56 +0200

    Method destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   layout false
-  # curl -d "name=Iphone11&price=1100" -X POST http://localhost:5000/items
+  # curl -d "name=Iphone11&price=1100&description=Ok" -X POST http://localhost:5000/items
   skip_before_action :verify_authenticity_token
   def index
     @items = Item.all
@@ -14,14 +14,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  def new; end
+
   def show
-    unless(@item = Item.where(id: params[:id]).first)
+    unless(@item = Item.find_by(id: params[:id]))
       render body: "Page not found", status: 404
+    end
+  end
+
+  def destroy
+    item = Item.find_by(id: params[:id]).destroy
+    if item.destroyed?
+      redirect_to items_path
+    else
+      render body: item.errors, status: :unprocessable_entity
     end
   end
 
   private
   def items_params
-    params.permit(:name, :price)
+    params.permit(:name, :price, :weight, :description)
   end
 end
